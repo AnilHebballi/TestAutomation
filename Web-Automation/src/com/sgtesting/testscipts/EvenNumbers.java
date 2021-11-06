afd61e7642c3ee1e5579f839def99617a2f7e861
AnilHebballi <anilhebballikhk@gmail.com>
2021-11-06 12:15:45 +0530

    Even Numbers

---
+++ Web-Automation/src/com/sgtesting/testscipts/EvenNumbers.java
@@ -0,0 +1,20 @@
+package com.sgtesting.testscipts;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		 evenNum();
+
+	}
+	static void evenNum()
+	{
+		for(int i=0;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
